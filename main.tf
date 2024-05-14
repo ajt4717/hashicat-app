c742c758bf56800da35cf37631ae8aceb54b3737
ajt4717 <ajt4717@gmail.com>
2024-05-14 19:08:15 +0000

    Added Cloud Storage module

--- main.tf
+++ main.tf
@@ -7,15 +7,6 @@ terraform {
   }
 }
 
-
-module "cloud-storage" {
-  source     = "app.terraform.io/my-org-4717/cloud-storage/google"
-  version    = "3.4.1"
-  names      = ["hashicat-private"]
-  prefix     = var.prefix
-  project_id = var.project
-}
-
 provider "google" {
   project = var.project
   region  = var.region
@@ -127,4 +118,12 @@ resource "null_resource" "configure-cat-app" {
       host        = google_compute_instance.hashicat.network_interface.0.access_config.0.nat_ip
     }
   }
+}
+
+module "cloud-storage" {
+  source     = "terraform-google-modules/cloud-storage/google"
+  version    = "3.4.1"
+  names      = ["hashicat"]
+  prefix     = var.prefix
+  project_id = var.project
 }
